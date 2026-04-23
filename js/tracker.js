89a71ff4a3265b87d9b6932403e19886c116e8c6
NiconX7-sketch <nicodemuschool07@gmail.com>
2026-04-23 11:56:54 +0300

    Update tracker.js

--- js/tracker.js
+++ js/tracker.js
@@ -1,16 +1,14 @@
 // ============================================
-// VISITOR TRACKING SYSTEM
+// VISITOR TRACKING SYSTEM - FIXED VERSION
 // ============================================
 
 const VisitorTracker = {
     visitorId: null,
-    tracked: false,
     
     init: function() {
-        if (this.tracked) return;
+        console.log('Visitor Tracker initializing...');
         this.getOrCreateVisitorId();
         this.trackPageView();
-        this.tracked = true;
     },
     
     getOrCreateVisitorId: function() {
@@ -18,6 +16,9 @@ const VisitorTracker = {
         if (!id) {
             id = 'vis_' + Date.now() + '_' + Math.random().toString(36).substr(2, 9);
             localStorage.setItem('visitor_id', id);
+            console.log('New visitor ID created:', id);
+        } else {
+            console.log('Existing visitor ID:', id);
         }
         this.visitorId = id;
         return id;
@@ -26,43 +27,39 @@ const VisitorTracker = {
     getDeviceInfo: function() {
         const ua = navigator.userAgent;
         let device = 'Desktop';
-        let os = 'Unknown';
         let browser = 'Unknown';
         
         if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) device = 'Tablet';
         else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle/i.test(ua)) device = 'Mobile';
-        
-        if (ua.indexOf('Windows') > -1) os = 'Windows';
-        else if (ua.indexOf('Mac') > -1) os = 'MacOS';
-        else if (ua.indexOf('Linux') > -1) os = 'Linux';
-        else if (ua.indexOf('Android') > -1) os = 'Android';
-        else if (ua.indexOf('iOS') > -1 || ua.indexOf('iPhone') > -1) os = 'iOS';
         
         if (ua.indexOf('Chrome') > -1) browser = 'Chrome';
         else if (ua.indexOf('Firefox') > -1) browser = 'Firefox';
         else if (ua.indexOf('Safari') > -1) browser = 'Safari';
         else if (ua.indexOf('Edge') > -1) browser = 'Edge';
         
-        return { device, os, browser };
+        return { device, browser };
     },
     
     trackPageView: function() {
         if (!window.supabase) {
-            setTimeout(() => this.trackPageView(), 500);
+            console.log('Waiting for Supabase...');
+            setTimeout(() => this.trackPageView(), 1000);
             return;
         }
         
         const deviceInfo = this.getDeviceInfo();
         
-        // Track visitor
+        // Track visitor using upsert
         window.supabase.from('visitors').upsert({
             visitor_id: this.visitorId,
-            user_agent: navigator.userAgent,
             device_type: deviceInfo.device,
             browser: deviceInfo.browser,
-            os: deviceInfo.os,
             last_visit: new Date().toISOString()
-        }, { onConflict: 'visitor_id' }).catch(err => console.log('Visitor track error:', err));
+        }, { onConflict: 'visitor_id' }).then(result => {
+            console.log('Visitor tracked:', result);
+        }).catch(error => {
+            console.error('Visitor track error:', error);
+        });
         
         // Track page view
         window.supabase.from('page_views').insert([{
@@ -71,7 +68,11 @@ const VisitorTracker = {
             page_title: document.title,
             referrer: document.referrer || 'direct',
             viewed_at: new Date().toISOString()
-        }]).catch(err => console.log('Page view error:', err));
+        }]).then(result => {
+            console.log('Page view tracked:', result);
+        }).catch(error => {
+            console.error('Page view error:', error);
+        });
     }
 };
 
@@ -79,5 +80,13 @@ const VisitorTracker = {
 if (window.supabase) {
     VisitorTracker.init();
 } else {
-    document.addEventListener('supabaseReady', () => VisitorTracker.init());
+    document.addEventListener('supabaseReady', () => {
+        VisitorTracker.init();
+    });
+    // Also try after a short delay
+    setTimeout(() => {
+        if (window.supabase && !VisitorTracker.visitorId) {
+            VisitorTracker.init();
+        }
+    }, 2000);
 }
